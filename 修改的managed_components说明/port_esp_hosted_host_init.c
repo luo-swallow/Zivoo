b8c5209c69833a14a5d14d4f4a37603a39904698
luo-swallow <2730429757@qq.com>
2026-07-01 17:27:52 +0800

    Add files via upload

---
+++ 修改的managed_components说明/port_esp_hosted_host_init.c
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2024-2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_log.h"
+#include "esp_hosted.h"
+
+#include "port_esp_hosted_host_log.h"
+
+#include "esp_private/startup_internal.h"
+
+DEFINE_LOG_TAG(host_init);
+
+//注释构造函数自动初始化，移至 app_main 手动调用，避免启动内存耗尽崩溃
+//ESP_SYSTEM_INIT_FN(esp_hosted_host_init, BIT(0), 120)
+//static void __attribute__((constructor)) esp_hosted_host_init(void)
+//{
+//	ESP_LOGI(TAG, "ESP Hosted : Host chip_ip[%d]", CONFIG_IDF_FIRMWARE_CHIP_ID);
+//	ESP_ERROR_CHECK(esp_hosted_init());
+//}
+
+static void __attribute__((destructor)) esp_hosted_host_deinit(void)
+{
+	ESP_LOGI(TAG, "ESP Hosted deinit");
+	esp_hosted_deinit();
+}
